666809760986d9788326d0f2500d353ae281ebb1
Behzad Golshan <behzad@recruit.ai>
2017-05-09 10:01:09 -0700

    Update version number

--- packages/flexmatcher/setup.py
+++ packages/flexmatcher/setup.py
@@ -19,7 +19,7 @@ test_requirements = [
 
 setup(
     name='flexmatcher',
-    version='0.1.0',
+    version='0.2.0',
     description="FlexMatcher is a schema matching package in Python which handles the problem of matching multiple schemas to a single mediated schema.",
     long_description=readme + '\n\n' + history,
     author="BigGorilla Team",
